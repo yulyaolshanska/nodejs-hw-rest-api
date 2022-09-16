e63d794d19d4a2436117601f399067ad8329ddf1
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-09-16 15:41:43 +0300

    test

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'Test  kcc' })
+  res.json({ message: 'Tekkkst  kcc' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
