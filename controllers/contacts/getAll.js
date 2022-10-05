4ce6bc8f0485d9360df0e0f9993233c915929a5b
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-10-06 00:06:40 +0300

    Pagination

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,8 +1,14 @@
 const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res, next) => {
+  const { _id } = req.user;
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
   try {
-    const contactList = await Contact.find({});
+    const contactList = await Contact.find({ owner: _id }, "", {
+      skip,
+      limit: Number(limit),
+    }).populate("owner", "_id email subscription");
     res
       .status(200)
       .json({ status: "success", code: 200, data: { result: contactList } });
