649b420d458384430b7ee9e7f07f10590d2c8eaf
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-10-21 00:26:46 +0300

    sendEmail

---
+++ helpers/index.js
@@ -0,0 +1,7 @@
+const sendEmail = require("./sendEmail");
+const handleSchemaValidationErrors = require("./handleSchemaValidationErrors");
+
+module.exports = {
+  sendEmail,
+  handleSchemaValidationErrors,
+};
---
+++ helpers/sendEmail.js
@@ -0,0 +1,14 @@
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const sendEmail = async (data) => {
+  const msg = { ...data, from: "olshanskayulya@gmail.com" };
+  try {
+    await sgMail.send(msg);
+    return true;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = sendEmail;
