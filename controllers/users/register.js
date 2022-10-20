b14f1fefd12695ec717a66c35a67358b853b6eac
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-10-21 00:29:19 +0300

    verificationToken for Register

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -2,6 +2,8 @@ const { User } = require("../../models/users");
 const { Conflict } = require("http-errors");
 const bcrypt = require("bcrypt");
 const gravatar = require("gravatar");
+const { v4 } = require("uuid");
+const { sendEmail } = require("../../helpers");
 
 const register = async (req, res, next) => {
   try {
@@ -12,7 +14,20 @@ const register = async (req, res, next) => {
     }
     const avatarURL = gravatar.url(email);
     const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-    await User.create({ email, password: hashPassword, avatarURL });
+    const verificationToken = v4();
+    await User.create({
+      email,
+      password: hashPassword,
+      avatarURL,
+      verificationToken,
+    });
+    const msg = {
+      to: email,
+      subject: "Confirm your email",
+      html: `<a href="http://localhost:3000/api/users/verify/${verificationToken}">Click to confirm your email</a>`,
+    };
+    await sendEmail(msg);
+
     res.status(201).json({
       user: {
         email,
